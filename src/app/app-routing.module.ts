91db7c79df0ac6e5dd189ebaf3b61de63bc4f234
Darek <darekpiwko@outlook.com>
2021-05-21 11:55:36 +0200

    add init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {BlogDetailsComponent} from "./components/blog-details/blog-details.component";
+import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogHomeComponent,
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogDetailsComponent
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private url = 'https://blogtai.herokuapp.com';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll() {
+    return this.http.get(this.url + '/api/posts/');
+  }
+
+  getById(id: string | null) {
+    return this.http.get(this.url + '/api/posts/' + id);
+  }
+
+  getByText(data: any) {
+    return this.http.post(this.url + '/api/posts/', data);
+  }
+
+
+}
